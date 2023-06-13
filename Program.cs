ec80d3bc481af81682c537ccefe62fa5b8f4accb
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-13 13:58:52 -0600

    Character escape sequences

--- Program.cs
+++ Program.cs
@@ -6,22 +6,36 @@
 //Console.Write(" ");
 //Console.Write("You wrote your first lines of code.");
 
-string firstName;
+//string firstName;
 //firstName = "Leo";
 //Console.WriteLine(firstName);
 
 //string lastName = "Jacobson";
 //Console.WriteLine(lastName);
 
-firstName = "Bob";
-int messageNumber = 3;
-float temperature = 34.4F;
+//firstName = "Bob";
+//int messageNumber = 3;
+//float temperature = 34.4F;
 
-Console.Write("Hello, ");
-Console.Write(firstName);
-Console.Write("! ");
-Console.Write("You have ");
-Console.Write(messageNumber);
-Console.Write(" in your inbox. The temperature is ");
-Console.Write(temperature);
-Console.Write(" celsius.");
+//Console.Write("Hello, ");
+//Console.Write(firstName);
+//Console.Write("! ");
+//Console.Write("You have ");
+//Console.Write(messageNumber);
+//Console.Write(" in your inbox. The temperature is ");
+//Console.Write(temperature);
+//Console.Write(" celsius.");
+
+//Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+//Console.WriteLine("Invoice: 1021\t\tComplete!");
+//Console.WriteLine("Invoice: 1022\t\tComplete!");
+//Console.WriteLine("\nOutput Directory:\t");
+//Console.Write(@"c:\invoices");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+//Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+//Console.WriteLine(@"c:\invoices\app.exe -j");
+
+Console.WriteLine("\u307C\u304F\u306E\u306A\u307E\u3048\u306F\u30EC\u30AA\u3067\u3059.");
