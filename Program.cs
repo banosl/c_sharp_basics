9299680eefaf841d7bb71b50f6e08843da0de7b5
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-13 11:17:33 -0600

    Declaring variables and using them to forma sentance

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("The current time is " + DateTime.Now);
 
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+//Console.Write("Congratulations!");
+//Console.Write(" ");
+//Console.Write("You wrote your first lines of code.");
+
+string firstName;
+//firstName = "Leo";
+//Console.WriteLine(firstName);
+
+//string lastName = "Jacobson";
+//Console.WriteLine(lastName);
+
+firstName = "Bob";
+int messageNumber = 3;
+float temperature = 34.4F;
+
+Console.Write("Hello, ");
+Console.Write(firstName);
+Console.Write("! ");
+Console.Write("You have ");
+Console.Write(messageNumber);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
