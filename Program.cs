b2d506d10be75f97e3bdd5316bd4d5e8ffc3543a
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-13 19:42:16 -0600

    adds example of interpolation

--- Program.cs
+++ Program.cs
@@ -38,4 +38,12 @@
 // User command to run an application
 //Console.WriteLine(@"c:\invoices\app.exe -j");
 
-Console.WriteLine("\u307C\u304F\u306E\u306A\u307E\u3048\u306F\u30EC\u30AA\u3067\u3059.");
+// Console.WriteLine("\u307C\u304F\u306E\u306A\u307E\u3048\u306F\u30EC\u30AA\u3067\u3059.");
+
+//Interpolation
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine($"View English output:\n  c:\\Exercise\\{projectName}\\data.txt");
+Console.WriteLine($"\n{russianMessage}:\n  c:\\Exercise\\{projectName}\\ru-RU\\data.txt");
