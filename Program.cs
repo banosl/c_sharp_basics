2913203e3ee715f46768d3fb82207821e846e486
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-16 09:35:59 -0600

    Adds student grade mini project

--- Program.cs
+++ Program.cs
@@ -73,22 +73,29 @@
 
 //More math
 
-int value = 1;
+// int value = 1;
 
-value = value + 1;
-Console.WriteLine("First increment: " + value);
+// value = value + 1;
+// Console.WriteLine("First increment: " + value);
 
-value += 1;
-Console.WriteLine("Second increment: " + value);
+// value += 1;
+// Console.WriteLine("Second increment: " + value);
 
-value++;
-Console.WriteLine("Third increment: " + value);
+// value++;
+// Console.WriteLine("Third increment: " + value);
 
-value = value - 1;
-Console.WriteLine("First decrement: " + value);
+// value = value - 1;
+// Console.WriteLine("First decrement: " + value);
 
-value -= 1;
-Console.WriteLine("Second decrement: " + value);
+// value -= 1;
+// Console.WriteLine("Second decrement: " + value);
 
-value--;
-Console.WriteLine("Third decrement: " + value);
+// value--;
+// Console.WriteLine("Third decrement: " + value);
+
+// Convert Fahrenheit to Celsius Challenge
+
+// int fahrenheit = 94;
+// decimal temp = ((decimal)fahrenheit - 32) * ((decimal)5 / (decimal)9);
+
+// Console.WriteLine($"The temperature is {temp} Celsius.");
